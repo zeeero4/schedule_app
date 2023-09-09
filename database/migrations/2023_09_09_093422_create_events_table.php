2ebe0cbe949becdb261b01a8b7f923f998455b6f
kzri4 <kazu4126ab@gmail.com>
2023-09-09 09:50:12 +0900

    モデル関連のファイル作成と、マイグレーションの実装

---
+++ database/migrations/2023_09_09_093422_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->string('title');
+            $table->string('body');
+            $table->string('start');
+            $table->string('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
